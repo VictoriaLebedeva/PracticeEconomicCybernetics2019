738a1d6dd15bbb585c82dcdf50943890c03a1bd8
VictoriaLebedeva <vict1464@gmail.com>
2019-02-19 16:07:28 +0300

    Update ShapeArc.java

--- Lebedeva/Lab1/ShapeArc.java
+++ Lebedeva/Lab1/ShapeArc.java
@@ -1,11 +1,15 @@
+package shapeTransformation;
+
 import java.awt.*;
 import java.awt.geom.*;
 
 public class ShapeArc implements Shape {
 
-    private double arcWidth, arcHeight, arcAngle;
-    double rotateAngle = 0;
-    Shape arc;
+    private double arcWidth;
+    private double arcHeight;
+    private double arcAngle;
+    private Shape arc;
+    private double rotateAngle = 0;
 
     public ShapeArc(double arcWidth, double arcHeight, double arcAngle) {
         assert (arcHeight < 0 || arcAngle < 0 || arcWidth < 0);
@@ -22,14 +26,54 @@ public class ShapeArc implements Shape {
                 Test.FRAME_HEIGHT / 2 - arcHeight / 2, arcWidth , arcHeight, 0 , arcAngle, 2 ));
     }
 
-    public Rectangle getBounds() {  return arc.getBounds(); }
-    public Rectangle2D getBounds2D() { return arc.getBounds2D(); }
-    public boolean contains(double x, double y) { return arc.contains(x,y); }
-    public boolean contains(Point2D p) { return arc.contains(p); }
-    public boolean intersects(double x, double y, double w, double h) { return arc.intersects(x, y, w, h); }
-    public boolean intersects(Rectangle2D r) { return arc.intersects(r); }
-    public boolean contains(double x, double y, double w, double h) { return arc.contains(x, y, w, h); }
-    public boolean contains(Rectangle2D r) { return arc.contains(r); }
-    public PathIterator getPathIterator(AffineTransform at) { return arc.getPathIterator(at); }
-    public PathIterator getPathIterator(AffineTransform at, double flatness) { return arc.getPathIterator(at, flatness); }
+
+    @Override
+    public Rectangle getBounds() {
+        return arc.getBounds();
+    }
+
+    @Override
+    public Rectangle2D getBounds2D() {
+        return arc.getBounds2D();
+    }
+
+    @Override
+    public boolean contains(double x, double y) {
+        return arc.contains(x, y);
+    }
+
+    @Override
+    public boolean contains(Point2D point2D) {
+        return arc.contains(point2D);
+    }
+
+    @Override
+    public boolean intersects(double x, double y, double w, double h) {
+        return arc.intersects(x, y, w, h);
+    }
+
+    @Override
+    public boolean intersects(Rectangle2D rectangle2D) {
+        return arc.intersects(rectangle2D);
+    }
+
+    @Override
+    public boolean contains(double x, double y, double width, double height) {
+        return arc.contains(x, y, width, height);
+    }
+
+    @Override
+    public boolean contains(Rectangle2D rectangle2D) {
+        return arc.contains(rectangle2D);
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform affineTransform) {
+        return arc.getPathIterator(affineTransform);
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform affineTransform, double flatness) {
+        return arc.getPathIterator(affineTransform, flatness);
+    }
 }
